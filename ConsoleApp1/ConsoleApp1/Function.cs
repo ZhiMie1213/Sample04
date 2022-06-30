473df0e4bca7535bd2eb717f60c6e0b839b353f1
FuNeKoU <87838131+ZhiMie1213@users.noreply.github.com>
2022-06-30 15:55:06 +0900

    SampleCode04

---
+++ ConsoleApp1/ConsoleApp1/Function.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace S04
+{
+    internal class Function
+    {
+        public static int Square( int value)
+        {
+            int result = value * value;
+            return result;
+        }
+
+        public static int Cube( int value )
+        {
+            int result = value * value * value;
+            return result;
+        }
+        public static int Power( long value, long power )
+        {
+            long result = 1;
+            for (int i = 0; i < value; i++)
+            {
+                result = value * value;
+            }
+            return result;
+        }
+
+        public static double WaitInputParseToDouble( )
+        {
+            double result = 0;
+            bool parseSuccess = false;
+
+            while ( !parseSuccess )
+            {
+                string userWeight = Console.ReadLine() ?? "";
+                parseSuccess = double.TryParse( Console.ReadLine(), out result);
+
+                if ( !parseSuccess )
+                    Console.WriteLine( "数値を入力してください" );
+            }
+                return result;
+        }
+    }
+}
--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,2 +1,63 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using S04;
+
+//ハンズオン01
+//int a = 10;
+//int b = S04.Function.Square( a );
+//Console.WriteLine( b );
+
+//int a = 3; long b = 20;
+//int c = Function.Power( a, b );
+//int d = Function.Power( a, 5 );
+//Console.WriteLine( d );
+
+//ハンズオン02
+/*
+Console.WriteLine( "体重を入力してください(kg)" );
+double weight = Function.WaitInputParseToDouble( );
+Console.WriteLine( "体重は" + weight.ToString( "F2" ) + "です" );
+
+Console.WriteLine( "身長を入力してください(cm)" );
+double height = Function.WaitInputParseToDouble( );
+Console.WriteLine( "身長は" + height.ToString( "F2" ) + "です" );
+
+const tmi = weight / ( height * height );
+Console.WriteLine( "BMIは" + tmi.ToString( "F4" ) + "です" );
+*/
+
+for ( int i = 0; i < args.Length; i++ ){
+    Console.WriteLine( "オプション" + i + ":" + args[ i ] ); 
+}
+
+//ハンズオン04
+
+double weight = 0;
+bool weightOptionIsValid = false;
+if ( args.Length > 0 ){
+    string arg0 = args[ 0 ];
+    weightOptionIsValid =  double.TryParse( arg0, out weight );
+}
+
+if ( !weightOptionIsValid )
+{
+    Console.WriteLine( "体重を入力してください(kg)" );
+    weight = Function.WaitInputParseToDouble( );
+}
+Console.WriteLine("体重は" + weight.ToString("F2") + "です");
+
+double height = 0;
+bool heightOptionIsValid = false;
+if( args.Length > 0 )
+{
+    string arg1 = args[ 0 ];
+    heightOptionIsValid = double.TryParse( arg1, out height );
+}
+
+if(!heightOptionIsValid )
+{
+    Console.WriteLine("身長を入力してください(cm)");
+    height = Function.WaitInputParseToDouble();
+}
+Console.WriteLine("身長は" + height.ToString("F2") + "です");
+
+double tmi = weight / (height * height);
+Console.WriteLine("BMIは" + tmi.ToString("F4") + "です");
